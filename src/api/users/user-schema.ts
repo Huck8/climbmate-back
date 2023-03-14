98eb66e097cedccdad8caaefc71fcbd30f8db764
Ángel <angelmonclova@gmail.com>
2023-03-14 20:12:30 +0100

    Create user-schema model

--- src/api/users/user-schema.ts
+++ src/api/users/user-schema.ts
@@ -1,11 +1,13 @@
 import mongoose, { Schema } from 'mongoose';
 
 export interface User {
+  name: string;
   email: string;
   password: string;
 }
 
 const userSchema = new Schema<User>({
+  name: String,
   email: String,
   password: String,
 });
