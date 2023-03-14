01384d228560aaf320c57bba97af4f6dce33c363
Ángel <angelmonclova@gmail.com>
2023-03-14 20:26:25 +0100

    Create auth-controllers auth-router auth-types and auth-utils files

---
+++ src/api/auth/auth-controller.ts
@@ -0,0 +1,35 @@
+import { RequestHandler } from 'express';
+
+import logger from '../../logger.js';
+import { UserModel } from '../users/user-schema.js';
+import { AuthRequest, LoginResponse } from './auth-types.js';
+import { encryptPassword, generateJWTToken } from './auth-utils.js';
+
+export const loginUserController: RequestHandler<
+  unknown,
+  LoginResponse,
+  AuthRequest
+> = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const filterUser = {
+      email,
+      password: encryptPassword(password),
+    };
+
+    const existingUser = await UserModel.findOne(filterUser).exec();
+
+    if (existingUser === null) {
+      console.log('entro por aquí');
+      return res.sendStatus(404);
+    }
+
+    const tokenJWT = generateJWTToken(email);
+    res.status(201).json({
+      accessToken: tokenJWT,
+    });
+  } catch (err) {
+    logger.error(err);
+    res.sendStatus(500);
+  }
+};
