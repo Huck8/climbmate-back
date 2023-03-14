16854c0035294932a67a5ea2de4f7164b928abe2
Ángel <angelmonclova@gmail.com>
2023-03-14 12:42:45 +0100

    Modify app file

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
-app.disabled('x-powered-by');
 
+app.use(cors());
+
+app.disable('x-powered-by');
 app.get('/', (req, res) => {
-  res.json({ hello: 'Server is working!!' });
+  res.json('Server is working!!');
 });
+
 app.use(express.json());
 
 export default app;
