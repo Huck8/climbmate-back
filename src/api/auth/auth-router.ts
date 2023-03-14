26632d298432c3982bd79d3e40287719226d6535
Ángel <angelmonclova@gmail.com>
2023-03-14 21:00:57 +0100

    Created Joi validation with required imputs

--- src/api/auth/auth-router.ts
+++ src/api/auth/auth-router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { validate } from 'express-validation';
 import { loginUserController } from './auth-controller.js';
+import authValidation from './entry-validation.js';
 
 const router = express.Router();
 
-router.route('/login').post(loginUserController);
+router.route('/login').post(validate(authValidation), loginUserController);
 
 export default router;
