8632853ab26ff30bf3a31e60af5db96c221d7976
Ángel <angelmonclova@gmail.com>
2023-03-15 09:49:24 +0100

    Created auth utils with encryption password

--- src/api/auth/auth-utils.ts
+++ src/api/auth/auth-utils.ts
@@ -1,5 +1,7 @@
 import crypto from 'node:crypto';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const createCypher = () => {
   if (process.env.PASSWORD_ENCRYPTION_ALGORITHM === undefined) {
