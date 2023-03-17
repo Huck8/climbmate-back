8ae11c57113412514f5336e98aa485c1f5adc059
Ángel <angelmonclova@gmail.com>
2023-03-17 16:40:13 +0100

    Create auth-controller

--- src/api/auth/auth-controller.ts
+++ src/api/auth/auth-controller.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 
 import { UserModel } from '../users/user-schema.js';
+import { CustomHTTPError } from '../utils/errors/custom-http-error.js';
 import { AuthRequest, LoginResponse } from './auth-types.js';
 import { encryptPassword, generateJWTToken } from './auth-utils.js';
 
@@ -8,7 +9,7 @@ export const loginUserController: RequestHandler<
   unknown,
   LoginResponse,
   AuthRequest
-> = async (req, res) => {
+> = async (req, res, next) => {
   const { email, password } = req.body;
 
   const filterUser = {
@@ -19,7 +20,7 @@ export const loginUserController: RequestHandler<
   const existingUser = await UserModel.findOne(filterUser).exec();
 
   if (existingUser === null) {
-    return res.sendStatus(404);
+    return next(new CustomHTTPError(404, 'User or password does no exists'));
   }
 
   const tokenJWT = generateJWTToken(email);
