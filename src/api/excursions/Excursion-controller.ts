227771d4e9621eefb0eec0d47a6a4ba38b92da85
Ángel <angelmonclova@gmail.com>
2023-03-22 20:04:49 +0100

    Excursion-controller and Excursion-router

--- src/api/excursions/Excursion-controller.ts
+++ src/api/excursions/Excursion-controller.ts
@@ -13,3 +13,20 @@ export const createExcursionController: RequestHandler<
   await ExcursionModel.create(excursion);
   resp.status(201).json({ msg: 'Your excursion has been created!' });
 };
+
+const queryProjection = { __v: 0 };
+
+export const getAllExcursionsController: RequestHandler<
+  unknown,
+  Excursion[] | { msg: string }
+> = async (_req, res, next) => {
+  try {
+    const foundExcursions = await ExcursionModel.find(
+      {},
+      queryProjection,
+    ).exec();
+    res.json(foundExcursions);
+  } catch (error) {
+    next(error);
+  }
+};
