8efa79bf3376bb1477950059d788248386d245c4
Ángel <angelmonclova@gmail.com>
2023-03-22 17:02:25 +0100

    Modified app and user-schema

--- src/api/users/user-schema.ts
+++ src/api/users/user-schema.ts
@@ -4,16 +4,12 @@ export interface User {
   name: string;
   email: string;
   password: string;
-  imageURL: string | undefined;
 }
-
-export type UserRegistration = Pick<User, 'name' | 'email' | 'password'>;
 
 const userSchema = new Schema<User>({
   name: String,
   email: String,
   password: String,
-  imageURL: String,
 });
 
 export const UserModel = mongoose.model<User>('User', userSchema, 'users');
