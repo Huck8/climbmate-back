25a4c62e23d0bbf1c0489b4aff8ec814c607fa4b
Ángel <angelmonclova@gmail.com>
2023-03-24 13:43:46 +0100

    Modify app test  with supabase mock

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,32 @@
 import request from 'supertest';
 import app from './app.js';
 
+jest.mock('@supabase/supabase-js', () => {
+  const data = {
+    publicUrl: 'https://example.com/photo.jpg',
+  };
+  return {
+    createClient: jest.fn().mockImplementation(() => ({
+      storage: {
+        from: jest.fn().mockReturnValue({
+          upload: jest.fn().mockResolvedValue({
+            error: null,
+            data: {
+              ...data,
+            },
+          }),
+          getPublicUrl: jest.fn().mockReturnValue({
+            error: null,
+            data: {
+              ...data,
+            },
+          }),
+        }),
+      },
+    })),
+  };
+});
+
 describe('Given an app', () => {
   test('When the server starts, then the app should respond with a message', async () => {
     const res = await request(app).get('/');
